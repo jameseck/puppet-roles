86280a48b95d49f1cf8d02c9eaba9696bcc64208
James Eckersall <james.eckersall@gmail.com>
2015-03-20 15:04:13 +0000

    elbuild role

--- manifests/elbuild.pp
+++ manifests/elbuild.pp
@@ -3,6 +3,12 @@ class roles::elbuild (
 
   include '::profiles::base'
 
+  exec { 'yum groupinstall Development Tools':
+    unless   => "/usr/bin/yum grouplist \"Development Tools\" | /bin/grep \"^Installed [Gg]roups\"",
+    command  => "/usr/bin/yum -y groupinstall \"Development Tools\"",
+    provider => 'shell',
+  }
+
   package { 'Development Tools':
     ensure => installed,
   }
