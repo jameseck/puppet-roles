5f6cc48538f22580893f90be18bbfa37c370d428
James Eckersall <james.eckersall@gmail.com>
2015-03-28 08:15:37 +0000

    added rpi role

--- manifests/rpi.pp
+++ manifests/rpi.pp
@@ -4,9 +4,9 @@ class roles::rpi (
   include '::keepalived'
   keepalived::vrrp::instance { 'VI_DNS':
     interface         => 'eth0',
-    state             => 'MASTER',
+    state             => 'BACKUP',
     virtual_router_id => '50',
-    priority          => '101',
+    priority          => '100',
     auth_type         => 'PASS',
     auth_pass         => 'secret',
     virtual_ipaddress => [ '192.168.1.2/24' ],
