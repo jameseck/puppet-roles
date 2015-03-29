35e664a31573497decc7d5fc61e56ce32178281e
James Eckersall <james.eckersall@gmail.com>
2015-03-29 11:47:14 +0100

    added media role

---
+++ manifests/media.pp
@@ -0,0 +1,35 @@
+class roles::media (
+) {
+
+  # Describes the subsonic server
+
+  # We'll also put the build requirements on this server,
+  # since it's likely the only Ubuntu server I need
+
+  # https://github.com/EugeneKay/subsonic
+
+
+
+  mount { '/mnt/nasb':
+    fstype => 'nfs4',
+    atboot => true,
+  }
+
+  $package_list = [ 'openjdk-7-jdk', 'maven', 'lintian', ]
+
+
+  file { '/home/james/gittmp':
+    ensure => directory,
+    owner  => 'james',
+    group  => 'james',
+    mode   => '0755',
+  }
+
+  vcsrepo { '/home/james/gittmp/subsonic':
+    ensure   => present,
+    provider => 'git',
+    source   => 'https://github.com/EugeneKay/subsonic',
+  }
+
+
+}
