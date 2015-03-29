ea3cc5b40625a09c124a547971727521c7d39b48
James Eckersall <james.eckersall@gmail.com>
2015-03-29 17:15:51 +0100

    added leecher role

---
+++ manifests/leecher.pp
@@ -0,0 +1,24 @@
+class roles::leecher (
+) {
+
+  include '::git'
+  include '::nginx'
+
+  package { 'rtorrent':
+    ensure => installed,
+  }
+
+  vcsrepo { '/opt/rutorrent':
+    ensure   => present,
+    provider => 'git',
+    source   => 'https://github.com/Novik/ruTorrent',
+  } ->
+  nginx::resource::vhost { 'rutorrent':
+    www_root         => '/opt/rutorrent',
+    vhost_cfg_append => {
+      'include'   => 'scgi_params',
+      'scgi_pass' => 'localhost:5000',
+    },
+  }
+
+}
