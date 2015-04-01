db3ec5197d5dd4326d94a9f51e31ed24b63e6659
James Eckersall <james.eckersall@gmail.com>
2015-04-01 12:36:06 +0100

    leecher role

--- manifests/leecher.pp
+++ manifests/leecher.pp
@@ -3,6 +3,7 @@ class roles::leecher (
 
   include '::git'
   include '::nginx'
+  include '::profiles::nux_desktop'
 
   class { 'sabnzbd':
     user     => 'james',
@@ -18,7 +19,9 @@ class roles::leecher (
     ensure => installed,
   }
 
-  package { 'php':
+  $php_packages = [ 'php-xml', 'php-common', 'php', 'php-cli', 'php-fpm', 'php-process', 'php-pecl-apcu', 'php-pear', ]
+
+  package { $php_packages:
     ensure => installed,
   }
 
@@ -31,13 +34,59 @@ class roles::leecher (
     provider => 'git',
     user     => 'james',
     source   => 'https://github.com/Novik/ruTorrent',
-  } ->
+  }
+
   nginx::resource::vhost { 'rutorrent':
-    www_root            => '/opt/rutorrent',
+    ensure               => present,
+    server_name          => [ $::fqdn ],
+    www_root             => '/opt/rutorrent',
+    listen_port          => '80',
+    use_default_location => false,
+    vhost_cfg_append     => {
+      'try_files' => '$uri $uri/ =404',
+    },
+  }
+  nginx::resource::location { 'rutorrent_auth':
+    ensure         => present,
+    location       => '^~ /rutorrent',
+    vhost          => 'rutorrent',
+    location_alias => '/opt/rutorrent',
+  }
+  nginx::resource::location { 'rutorrent_php':
+    ensure              => present,
+    location            => '~ \.php$',
+    vhost               => 'rutorrent',
+    location_cfg_append => {
+      'fastcgi_split_path_info' => '^(.+\.php)(/.+)$',
+      'fastcgi_pass'            => '127.0.0.1:9000',
+      'fastcgi_param'           => 'SCRIPT_FILENAME $document_root$fastcgi_script_name',
+      'fastcgi_index'           => 'index.php',
+      'include'                 => 'fastcgi_params',
+    }
+  }
+  nginx::resource::location { 'rutorrent_RPC2':
+    ensure              => present,
+    location            => '/RPC2',
+    vhost               => 'rutorrent',
     location_cfg_append => {
       'include'   => 'scgi_params',
       'scgi_pass' => 'localhost:5000',
-    },
+    }
+  }
+
+  file { '/var/lib/php/session':
+    ensure => directory,
+    owner  => 'nginx',
+    group  => 'root',
+    mode   => '0770',
+  }
+
+  file { '/etc/php-fpm.d/www.conf':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/roles/leecher/php-fpm.d_www.conf',
   }
 
   package { 'nfs-utils':
