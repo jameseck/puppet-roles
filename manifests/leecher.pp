89066ec37f0b45a88f0258833e3043e6ceb3e28e
James Eckersall <james.eckersall@gmail.com>
2015-04-02 10:23:52 +0100

    leecher  role

--- manifests/leecher.pp
+++ manifests/leecher.pp
@@ -180,21 +180,21 @@ class roles::leecher (
     }
   }
 
-  nginx::resource::location { 'rutorrent_php':
-    ensure              => present,
-    location            => '~ rutorrent/\.php$',
-    vhost               => 'leecher',
-    ssl                 => true,
-    ssl_only            => true,
-    www_root            => '/opt/rutorrent',
-    location_cfg_append => {
-      'fastcgi_split_path_info' => '^(.+\.php)(/.+)$',
-      'fastcgi_pass'            => '127.0.0.1:9000',
-      'fastcgi_param'           => 'SCRIPT_FILENAME $document_root$fastcgi_script_name',
-      'fastcgi_index'           => 'index.php',
-      'include'                 => 'fastcgi_params',
-    }
-  }
+#  nginx::resource::location { 'rutorrent_php':
+#    ensure              => present,
+#    location            => '~ rutorrent/\.php$',
+#    vhost               => 'leecher',
+#    ssl                 => true,
+#    ssl_only            => true,
+#    www_root            => '/opt/rutorrent',
+#    location_cfg_append => {
+#      'fastcgi_split_path_info' => '^(.+\.php)(/.+)$',
+#      'fastcgi_pass'            => '127.0.0.1:9000',
+#      'fastcgi_param'           => 'SCRIPT_FILENAME $document_root$fastcgi_script_name',
+#      'fastcgi_index'           => 'index.php',
+#      'include'                 => 'fastcgi_params',
+#    }
+#  }
   nginx::resource::location { 'rutorrent_RPC2':
     ensure              => present,
     location            => '/rutorrent/RPC2',
