985eb48981c044e972ea85cf4b75683a0a93d81e
James Eckersall <james.eckersall@gmail.com>
2015-09-10 20:40:55 +0100

    keepalived foreman changes

--- manifests/foreman.pp
+++ manifests/foreman.pp
@@ -17,6 +17,13 @@ class roles::foreman (
   }
 
   include '::keepalived'
+  file { '/etc/keepalived/notify-keepalived.sh':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => template('roles/foreman/notify-keepalived.sh.erb'),
+  } ->
   keepalived::vrrp::instance { 'VI_DNS':
     interface         => 'eth0',
     state             => 'MASTER',
@@ -25,6 +32,8 @@ class roles::foreman (
     auth_type         => 'PASS',
     auth_pass         => 'secret',
     virtual_ipaddress => [ '192.168.1.2/24' ],
+    notify_script     => '/etc/keepalived/notify-keepalived.sh',
   }
+
 
 }
