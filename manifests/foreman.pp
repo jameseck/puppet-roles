6b889303376534b94c078fbda6951646bae26a90
James Eckersall <james.eckersall@gmail.com>
2015-09-10 21:18:20 +0100

    keepalived_dns profile for foreman and rpi

--- manifests/foreman.pp
+++ manifests/foreman.pp
@@ -16,37 +16,6 @@ class roles::foreman (
     content  => 'Defaults:foreman-proxy !requiretty',
   }
 
-  include '::keepalived'
-  file { '/etc/keepalived/notify-keepalived.sh':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-    content => template('roles/foreman/notify-keepalived.sh.erb'),
-  } ->
-  file { '/etc/keepalived/check-keepalived.sh':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/roles/foreman/check-keepalived.sh',
-  } ->
-  keepalived::vrrp::instance { 'VI_DNS':
-    interface         => 'eth0',
-    state             => 'MASTER',
-    virtual_router_id => '50',
-    priority          => '101',
-    auth_type         => 'PASS',
-    auth_pass         => 'secret',
-    virtual_ipaddress => [ '192.168.1.2/24' ],
-    notify_script     => '/etc/keepalived/notify-keepalived.sh',
-    track_script      => 'checkscript',
-  } ->
-  keepalived::vrrp::script { 'checkscript':
-    script   => '/etc/keepalived/check-keepalived.sh',
-    interval => 2,
-    rise     => 2,
-    fall     => 2,
-  }
+  include '::profiles::keepalived_dns'
 
 }
--- manifests/rpi.pp
+++ manifests/rpi.pp
@@ -1,15 +1,6 @@
 class roles::rpi (
 ) {
 
-  include '::keepalived'
-  keepalived::vrrp::instance { 'VI_DNS':
-    interface         => 'eth0',
-    state             => 'BACKUP',
-    virtual_router_id => '50',
-    priority          => '100',
-    auth_type         => 'PASS',
-    auth_pass         => 'secret',
-    virtual_ipaddress => [ '192.168.1.2/24' ],
-  }
+  include '::profiles::keepalived_dns'
 
 }
