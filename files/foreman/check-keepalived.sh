68a1dd8fb7a57728d992a4d84eabd7c08d048aee
James Eckersall <james.eckersall@gmail.com>
2015-09-10 20:53:50 +0100

    keepalived foreman changes

---
+++ files/foreman/check-keepalived.sh
@@ -0,0 +1,1 @@
+dig @`facter ipaddress` google.com
--- manifests/foreman.pp
+++ manifests/foreman.pp
@@ -24,6 +24,13 @@ class roles::foreman (
     mode    => '0755',
     content => template('roles/foreman/notify-keepalived.sh.erb'),
   } ->
+  file { '/etc/keepalived/check-keepalived.sh':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => 'puppet:///modules/roles/foreman/check-keepalived.sh',
+  } ->
   keepalived::vrrp::instance { 'VI_DNS':
     interface         => 'eth0',
     state             => 'MASTER',
@@ -33,7 +40,12 @@ class roles::foreman (
     auth_pass         => 'secret',
     virtual_ipaddress => [ '192.168.1.2/24' ],
     notify_script     => '/etc/keepalived/notify-keepalived.sh',
+  } ->
+  keepalived::vrrp::script { 'check':
+    script   => '/etc/keepalived/check-keepalived.sh',
+    interval => 2,
+    rise     => 2,
+    fall     => 2,
   }
-
 
 }
